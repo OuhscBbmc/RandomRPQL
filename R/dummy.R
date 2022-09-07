919afb9ed32a5bb7963bf642d26a04d86d15c2cf
Will Beasley <wibeasley@hotmail.com>
2022-09-07 12:07:17 -0500

    more documentation examples
    close #1
    
    Co-Authored-By: Yutian Thompson <42153791+yutiantang@users.noreply.github.com>

--- R/dummy.R
+++ R/dummy.R
@@ -2,13 +2,16 @@
 #' Example title is an incomplete sentence --no period
 #'
 #' @description
-#' "Description" is a series of complete sentence
+#' "Description" is a series of complete sentences.
+#' See https://r-pkgs.org/man.html.
+#' Keep these documentation lines under 80 or 100 characters
+#' (I forgot which )
 #'
-#' @param x Integer to increment Required.
-#' @param y Amount to increment.  Defaults to 1. Required.
+#' @param x Integer to increment.  Required.
+#' @param y Amount to increment.  Defaults to 1. Required integer.
 #'
 #' @return
-#' An [base::integer]
+#' A [base::integer] that is incremented by `y`.
 #'
 #' @details
 #' Adds one.
@@ -23,11 +26,18 @@
 #' dummy(0L)
 #' dummy(1L)
 #' dummy(0L, 2L)
-
+#'
+#' @importFrom rlang .data
 #' @export
 dummy <- function(x, y = 1L) {
   checkmate::assert_integer(x, any.missing = TRUE, len = 1)
   checkmate::assert_integer(y, any.missing = TRUE, len = 1)
+
+  d <-
+    mtcars |>
+    dplyr::select(
+      .data$hp,           # In dplyr verbs, prefix most variables names with `.data` to avoid warnings from R CMD check.
+    )
 
   x + y
 }
