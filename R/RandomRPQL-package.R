5c8c9ecc7f7b2f9ed7c613609240c24380b73277
Will Beasley <wibeasley@hotmail.com>
2022-09-07 11:32:58 -0500

    more package skeleton
    ref #1
    
    Co-Authored-By: Yutian Thompson <42153791+yutiantang@users.noreply.github.com>

---
+++ R/RandomRPQL-package.R
@@ -0,0 +1,67 @@
+#' @docType package
+#' @name RandomRPQL-package
+#' @aliases
+#' RandomRPQL
+#'
+#' @title Variable selection method for GLMMs
+#'
+#' @description
+#' A variable selection method for generalize linear mixed-effect models.
+#'
+#' TODO: replace with the project's funding
+#'
+#'
+#'
+#' Much of this package has been developed to support the needs of the
+#' following projects.  We appreciate the support.
+#' * *OUHSC CCAN Independent Evaluation of the State of Oklahoma Competitive
+#' Maternal, Infant, and Early Childhood Home Visiting
+#' ([MIECHV](https://mchb.hrsa.gov/maternal-child-health-initiatives/home-visiting-overview)) Project*.
+#' HRSA/ACF D89MC23154.
+#' David Bard, PI, OUHSC; 2011-2015.
+#' * *Independent Evaluation of the State of OK MIECHV Evidence Based Home
+#' Visitation Project*, [NIH](https://www.nih.gov/)-sponsored collaboration
+#' with [OSDH](https://oklahoma.gov/health.html).
+#' David Bard, PI, OUHSC; 2015-2017.
+#' * *OSDH ParentPRO Pilot Evaluation*, federally-sponsored collaboration
+#' with [OSDH](https://oklahoma.gov/health.html).
+#' David Bard, PI, OUHSC; 2015-2017.
+#' * *Title IV-E Waiver Project*, [HRSA/MCHB](https://mchb.hrsa.gov/)-sponsored
+#' collaboration with [OKDHS](https://oklahoma.gov/okdhs.html);
+#' David Bard, PI, OUHSC; 2014-2017.
+#' * *Integrative Analysis of Longitudinal Studies of Aging (IALSA)*,
+#' sponsored by [NIH 5P01AG043362](https://grantome.com/grant/NIH/P01-AG043362-05).
+#' Scott Hofer, PI, University of Victoria;
+#' Will Beasley, PI of site-award, OUHSC; 2013-2018.
+#' * *Oklahoma Shared Clinical and Translational Resources*, sponsored by
+#' [NIH NIGMS; U54 GM104938](https://grantome.com/grant/NIH/U54-GM104938).
+#' Judith A. James, PI, OUHSC; 2013-2018.
+#' * Additional Institutional Support from OUHSC
+#' [Dept of Pediatrics](https://medicine.ouhsc.edu/Academic-Departments/Pediatrics);
+#' 2013-2017.
+#'
+#'
+#' @note
+#' The release version is not yet available.
+#' It will be available through
+#' [CRAN](https://cran.r-project.org/package=RandomRPQL) by running
+#' `install.packages('RandomRPQL')`.  The most recent development version is
+#' available through [GitHub](https://github.com/OuhscBbmc/RandomRPQL) by
+#' running `remotes::install_github('OuhscBbmc/RandomRPQL')`
+#' (make sure [remotes](https://cran.r-project.org/package=remotes) is
+#' already installed). If you're having trouble with the package, please
+#' install the development version.  If this doesn't solve your problem,
+#' please create a [new issue](https://github.com/OuhscBbmc/RandomRPQL/issues),
+#' or email Will.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' # Install/update RandomRPQL with the release version from CRAN.
+#' # install.packages('RandomRPQL')
+#'
+#' # Install/update RandomRPQL with the development version from GitHub
+#' # install.packages("remotes") # Uncomment if `remotes` isn't installed already.
+#' # remotes::install_github('OuhscBbmc/RandomRPQL')
+#' }
+NULL
